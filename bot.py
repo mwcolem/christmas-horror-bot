1babd50a3c6424bc67242432b4cc8eab7d7e23af
mwcolem@gmail.com <mwcolem@gmail.com>
2018-10-24 23:16:33 -0400

    Successfully posts to reddit

--- bot.py
+++ bot.py
@@ -2,12 +2,18 @@ import json
 from urllib.request import Request
 from imdb import IMDb
 import praw
+import configparser
 
 ia = IMDb()
+config = configparser.ConfigParser()
+config.read('config.ini')
+
 reddit = praw.Reddit(
-        client_id="",
-        client_secret="",
-        user_agent=""
+        client_id=config['reddit']['client_id'],
+        client_secret=config['reddit']['client_secret'],
+        user_agent=config['reddit']['user_agent'],
+        username=config['reddit']['username'],
+        password=config['reddit']['password']
         )
 
 with open("movies.txt", "r") as movies:
@@ -15,4 +21,9 @@ with open("movies.txt", "r") as movies:
         title = movie.rstrip("\n")
         print(ia.search_movie(title))
 
+print(reddit.read_only)
 
+for submission in reddit.subreddit('christmashorror').hot(limit=10):
+    print(submission.title)
+
+reddit.subreddit('christmashorror').submit("test2", "this is also a test submission")
